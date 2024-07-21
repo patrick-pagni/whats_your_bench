2ba4a4ca6ba3ec4a2ded2836c60418c1ff17f1fe
Patrick Pagni <patrick.pagni1@gmail.com>
2024-07-21 17:41:51 +0100

    Added models for problem 1 for stan and pyro

--- whats_your_bench/problem_set.py
+++ whats_your_bench/problem_set.py
@@ -1,6 +1,18 @@
 from whats_your_bench import conjugate_priors as cp
 from scipy import stats
+
+"""
+Import for PyMC
+"""
 import pymc as pm
+
+"""
+Imports for pyro
+"""
+import torch
+import pyro
+import pyro.distributions as pyro_dist
+
 
 class Problem():
 
@@ -21,24 +33,59 @@ class Problem():
 
 class Problem1(Problem):
 
+    """
+    First problem in problem set.
+
+    Challenge: Given a random data set with a known variance, find the mean of the posterior.
+
+    Parameters:
+        Variance: 3
+        Conjugate prior:
+            Normal(mean = 3, variance = 1)
+        PPL Priors [Uninformative Prior]:
+            Normal(mean = 0, variance = 1)
+        Data characterisitcs:
+            Random sample from Normal(mean = 3, variance = 1)
+            N = 100
+    """
+
     def __init__(self):
         super().__init__(
             cp.NormalKnownVar(3, [5, 3]),
             [0, 1],
-            10,
+            100,
             stats.norm(3, 4)
         )
 
         self.ppl_mu, self.ppl_sigma = self.ppl_priors
 
     def _pymc_model(self):
-        with pm.Model() as m:
+        with pm.Model() as model:
 
             mu = pm.Normal("mu", mu = self.ppl_mu, sigma = self.ppl_sigma)
             obs = pm.Normal("obs", mu = mu, sigma = self.conjugate_model.sigma, observed = self.data)
 
-            idata = pm.sample_prior_predictive()
-            idata.extend(pm.sample())
-            pm.sample_posterior_predictive(idata, extend_inferencedata=True)
+            idata = pm.sample()
 
-        return idata
+            return float(idata.posterior["mu"].mean())
+        
+    def _build_pyro_model(self):
+        mu = pyro.sample("mu", pyro_dist.Normal(self.ppl_mu, self.ppl_sigma))
+
+        with pyro.plate("data", self.data.shape[0]):
+            pyro.sample("obs", pyro_dist.Normal(mu, self.conjugate_model.sigma), obs = torch.Tensor(self.data))
+
+    def _pyro_model(self):
+        kernel = pyro.infer.NUTS(self._build_pyro_model)
+
+        mcmc = pyro.infer.MCMC(kernel, num_samples = 1000, warmup_steps = 200)
+        mcmc.run()
+
+        hmc_samples = {k: v.detach().cpu().numpy() for k, v in mcmc.get_samples().items()}
+
+        return hmc_samples["mu"].mean()
+
+
+        
+
+
---
+++ whats_your_bench/stan_models/normalKnownVar.stan
@@ -0,0 +1,14 @@
+
+    data {
+    int<lower=0> N;
+    array[N] real X;
+    }
+
+    parameters {
+    real mu;
+    }
+
+    model {
+    mu ~ normal(0, 1);  // uniform prior on interval 0,1
+    X ~ normal(mu, 3);
+    }
